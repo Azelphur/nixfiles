2d84c7c57533da1a22089bf84168317eb51cc044
azelphur <azelphur@azelphur.com>
2024-09-16 10:44:54 +0100

    Do zsh things

--- modules/home-manager/zsh/zsh.nix
+++ modules/home-manager/zsh/zsh.nix
@@ -1,6 +1,11 @@
 { config, pkgs, ... }:
 
 {
+  home.packages = with pkgs; [
+    python312Packages.pygments
+    fzf
+    thefuck
+  ];
   programs.zsh = {
     enable = true;
     history = {
@@ -16,13 +21,19 @@
       hl = "heroku logs --tail -a";
       hr = "heroku run -a";
       random = "openssl rand -hex 12";
+      b = "sudo nixos-rebuild switch";
     };
+    enableCompletion = true;
     autosuggestion = {
+      enable = true;
+    };
+    syntaxHighlighting = {
       enable = true;
     };
     oh-my-zsh = {
       enable = true;
       theme = "fox";
+      plugins = ["colorize" "thefuck" "colored-man-pages" "copypath" "cp" "docker" "extract" "fzf" "heroku" "sudo" "git" "zsh-interactive-cd"];
     };
     initExtra = "fastfetch --logo .assets/nixos.png --logo-type kitty-direct --logo-width 40 --logo-height 20";
   };
