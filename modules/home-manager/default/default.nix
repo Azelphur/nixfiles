5344c55eee6c46a8aea7d3405b808b0c545ffb61
azelphur <azelphur@azelphur.com>
2024-09-28 20:48:13 +0100

    Add some applications

--- modules/home-manager/default/default.nix
+++ modules/home-manager/default/default.nix
@@ -41,6 +41,10 @@
     # '')
     # Must be ran with WEBKIT_DISABLE_DMABUF_RENDERER=1 until graphics driver update
     orca-slicer
+    nextcloud-client
+    lcov
+    dig
+    mtr
     nmap
     tigervnc
     pwvucontrol
