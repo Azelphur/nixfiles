46f4cae322e1e2cb55c398a659b6e7a3d9852415
azelphur <azelphur@azelphur.com>
2024-10-12 20:13:43 +0100

    Conky for framework

--- hosts/azelphur-framework/home.nix
+++ hosts/azelphur-framework/home.nix
@@ -1,13 +1,17 @@
-{ config, pkgs, inputs, ... }:
+{ config, pkgs, inputs, lib, ... }:
 
 {
   imports = [
     ../../modules/home-manager/default/default.nix
     inputs.nixvim.homeManagerModules.nixvim
   ];
+  home.packages = with pkgs; [
+      conky
+  ];
   wayland.windowManager.hyprland.settings = {
     exec-once = [
       "nm-applet"
+      "conky"
     ];
     bindd = [] ++ (
       # workspaces
@@ -31,6 +35,15 @@
   # manage.
   home.username = "azelphur";
   home.homeDirectory = "/home/azelphur";
+  stylix.targets.hyprpaper.enable = lib.mkForce false;
+  services.hyprpaper.settings = {
+    preload = [
+      "~/.wallpaper.png"
+    ];
+    wallpaper = [
+      "eDP-1,~/.wallpaper.png"
+    ];
+  };
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
@@ -44,6 +57,8 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
+    ".wallpaper.png".source = ./wallpaper.png;
+    ".config/conky/conky.conf".source = ./conky.conf;
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
