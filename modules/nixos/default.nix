7e5093c9e468c63c3309808a1b9792c43768e23b
azelphur <azelphur@azelphur.com>
2025-05-27 18:06:31 +0100

    Garbage collection

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -38,6 +38,11 @@
     substituters = ["https://hyprland.cachix.org"];
     trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
   };
+  nix.gc = {
+    automatic = true;
+    randomizedDelaySec = "14m";
+    options = "--delete-older-than 10d";
+  };
   programs.git = {
     enable = true;
     config = {
