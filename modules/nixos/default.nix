678f12fe9a13992ddc6eebaac85ab0672687672c
azelphur <azelphur@azelphur.com>
2025-06-05 23:38:00 +0100

    Add go-hass-agent

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,5 +1,44 @@
 { config, lib, pkgs, inputs, ...}:
 
+let
+  go-hass-agent = pkgs.buildGoModule (finalAttrs: {
+    pname = "go-hass-agent";
+    version = "13.2.7";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "joshuar";
+      repo = "go-hass-agent";
+      tag = "v${finalAttrs.version}";
+      hash = "sha256-nec5gH/p65sfxNDD+5TEc7POP7B2MOA23aMqJ9aXAz8=";
+    };
+
+    nativeBuildInputs = with pkgs; [
+      pkg-config
+    ];
+
+    subPackages = [ "." ];
+
+    buildInputs = with pkgs; [
+      xorg.libX11
+      xorg.libXrandr
+      xorg.libXxf86vm
+      xorg.libXi
+      xorg.libXcursor
+      xorg.libXinerama
+      xorg.libXext
+      xorg.libxcb
+      mesa
+      glfw
+    ];
+
+    vendorHash = "sha256-PVicvYGaZxNVUbmNCEGUd4BZklhgaSMKpeiy898YTbM=";
+    meta = with pkgs.lib; {
+      description = "Go-based Home Assistant agent";
+      homepage = "https://github.com/joshuar/go-hass-agent";
+      license = licenses.mit;
+    };
+  });
+in
 {
   imports = [
     inputs.home-manager.nixosModules.default
@@ -122,6 +161,7 @@
     git
     wget
     killall
+    go-hass-agent
     (python3.withPackages(ps: with ps; [ 
       requests
       virtualenv
