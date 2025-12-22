a925c54594d7d5c3392409079ad164d72d1e8c1f
azelphur <azelphur@azelphur.com>
2025-12-22 16:16:46 +0000

    Add udev rule to make Keychron configurator work.

--- hosts/azelphur-pc/configuration.nix
+++ hosts/azelphur-pc/configuration.nix
@@ -17,6 +17,13 @@
     defaultRuntime = true; # Register as default OpenXR runtime
     highPriority = true;
   };
+
+  # udev rule for Keychron Q6 HE, without it, the configuration tool does not work.
+  services.udev.extraRules = ''
+    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="3434", ATTRS{idProduct}=="0b61", MODE="0660", GROUP="users", TAG+="uaccess", TAG+="udev-acl"
+  '';
+
+  # udev rules for wolf, this can't go in extraRules as it needs to execute before 99.
   services.udev.packages = lib.singleton (pkgs.writeTextFile
   { name = "wolf-virtual-inputs";
     text = ''
