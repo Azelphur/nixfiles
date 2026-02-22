4451c657f040c92690e47a75c3b1200496c2692c
azelphur <azelphur@azelphur.com>
2026-02-22 14:23:59 +0000

    Implement some feedback from mainainers.EpicEric, thanks :)

--- pkgs/go-hass-agent.nix
+++ pkgs/go-hass-agent.nix
@@ -1,27 +1,25 @@
-{ lib, pkgs, ...}:
+{ lib, pkgs, buildNpmPackage, buildGoModule, fetchFromGitHub, ...}:
 
 let
   pname = "go-hass-agent";
   version = "14.8.0";
 
-  src = pkgs.fetchFromGitHub {
+  src = fetchFromGitHub {
     owner = "joshuar";
     repo = "go-hass-agent";
     tag = "v${version}";
     hash = "sha256-MHQCJLvy3YnDeXal29+zwSAYCeVCisa9se1+GCIiIvw=";
   };
 
-  nodeAssets = pkgs.buildNpmPackage {
+  nodeAssets = buildNpmPackage {
     pname = "go-hass-agent-assets";
     inherit src version;
 
     npmDepsHash = "sha256-rScsGZMdyd8chY380MxZEA6OkwqkH46LlvjCTBOohfE=";
     buildPhase = ''
       runHook preBuild
-
       npm run build:js
       npm run build:css
-
       runHook postBuild
     '';
     installPhase = ''
@@ -31,7 +29,7 @@ let
       runHook postInstall
     '';
   };
-in pkgs.buildGoModule (finalAttrs: {
+in buildGoModule (finalAttrs: {
   inherit pname version src;
 
   preBuild = ''
@@ -46,9 +44,18 @@ in pkgs.buildGoModule (finalAttrs: {
 
   vendorHash = "sha256-D90MuhN8TwzISqT/c26Fk2HFsSsB5k9WOcS4olZuQ3w=";
 
-  meta = with pkgs.lib; {
+  meta = with lib; {
     description = "Go-based Home Assistant agent";
     homepage = "https://github.com/joshuar/go-hass-agent";
+    changelog = "https://github.com/joshuar/go-hass-agent/releases/tag/v${finalAttrs.version}";
+    downloadPage = "https://github.com/joshuar/go-hass-agent/";
+    longDescription = ''
+      Go Hass Agent is an application to expose sensors, controls, and events
+      from a device to Home Assistant. You can think of it as something similar
+      to the Home Assistant companion app for mobile devices, but for your
+      desktop, server, Raspberry Pi, Arduino, toaster, whatever. If it can run
+      Go and Linux, it can run Go Hass Agent!
+    '';
     license = licenses.mit;
     mainProgram = "go-hass-agent";
     platforms = platforms.linux;
