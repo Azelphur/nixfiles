dcac1e8903d62d5ec95f5ded7362b49e65f788c2
azelphur <azelphur@azelphur.com>
2026-02-22 10:19:11 +0000

    Fix up go-hass-agent package, much cleaner now

--- pkgs/go-hass-agent.nix
+++ pkgs/go-hass-agent.nix
@@ -1,18 +1,19 @@
 { lib, pkgs, ...}:
 
 let
+  pname = "go-hass-agent";
+  version = "14.8.0";
+
   src = pkgs.fetchFromGitHub {
     owner = "joshuar";
     repo = "go-hass-agent";
-    tag = "v14.8.0";
-
+    tag = "v${version}";
     hash = "sha256-MHQCJLvy3YnDeXal29+zwSAYCeVCisa9se1+GCIiIvw=";
   };
 
   nodeAssets = pkgs.buildNpmPackage {
     pname = "go-hass-agent-assets";
-    version = "14.8.0";
-    inherit src;
+    inherit src version;
 
     npmDepsHash = "sha256-rScsGZMdyd8chY380MxZEA6OkwqkH46LlvjCTBOohfE=";
     buildPhase = ''
@@ -26,39 +27,15 @@ let
     installPhase = ''
       runHook preInstall
       mkdir -p $out
-      cp -r web $out/web
-      cp -r node_modules $out/node_modules
+      cp -r web $out/
       runHook postInstall
     '';
   };
 in pkgs.buildGoModule (finalAttrs: {
-  pname = "go-hass-agent";
-  version = "14.8.0";
-  inherit src;
+  inherit pname version src;
 
-  nativeBuildInputs = with pkgs; [
-    pkg-config
-    git
-    breakpointHook
-  ];
-
-  subPackages = [ "." ];
-
-  buildInputs = with pkgs; [
-    xorg.libX11
-    xorg.libXrandr
-    xorg.libXxf86vm
-    xorg.libXi
-    xorg.libXcursor
-    xorg.libXinerama
-    xorg.libXext
-    xorg.libxcb
-    mesa
-    glfw
-  ];
   preBuild = ''
-    cp -r ${nodeAssets}/web/* ./web
-    cp -r ${nodeAssets}/node_modules ./node_modules
+    cp -r ${nodeAssets}/web .
   '';
 
   ldflags = [
@@ -68,9 +45,12 @@ in pkgs.buildGoModule (finalAttrs: {
   ];
 
   vendorHash = "sha256-D90MuhN8TwzISqT/c26Fk2HFsSsB5k9WOcS4olZuQ3w=";
+
   meta = with pkgs.lib; {
     description = "Go-based Home Assistant agent";
     homepage = "https://github.com/joshuar/go-hass-agent";
     license = licenses.mit;
+    mainProgram = "go-hass-agent";
+    platforms = platforms.linux;
   };
 })
