1a1be95c1ae1a304bf1d27174dfb1ec581e3293b
azelphur <azelphur@azelphur.com>
2026-06-06 20:59:30 +0100

    Add keychron C3 udev rule

--- modules/nixos/hardware/keychron.nix
+++ modules/nixos/hardware/keychron.nix
@@ -3,6 +3,9 @@
 {
   # udev rule for Keychron Q6 HE, required to make the keychron configurator work
   services.udev.extraRules = ''
+    # Keychron Q6 HE
     KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="3434", ATTRS{idProduct}=="0b61", MODE="0660", GROUP="users", TAG+="uaccess", TAG+="udev-acl"
+    # Keychron C3 Pro
+    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="3434", ATTRS{idProduct}=="0431", MODE="0660", GROUP="users", TAG+="uaccess", TAG+="udev-acl"
   '';
 }
